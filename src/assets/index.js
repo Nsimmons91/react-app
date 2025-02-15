5360de8a621e72d04c830e63dc0e47ee5df08096
Nina Simmons <pkpbabies@gmail.com>
2025-02-14 19:30:21 -0500

    Debug, add images

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react";
-import "./index.css";
-import RockPaperScissorsGame from "../App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import RockPaperScissorsGame from './App';
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import RockPaperScissorsGame from './App';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);  
+
+root.render(
   <React.StrictMode>
     <RockPaperScissorsGame/>
   </React.StrictMode>,
-  document.getElementById('root')
 );
